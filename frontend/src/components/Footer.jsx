7559e65048b10ae1f1c4342ce1f70c835689b82f
Thejas663 <yst.thejas@gamil.com>
2025-06-13 10:27:47 +0530

    commit+1

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaLocationDot } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
@@ -7,7 +8,15 @@ const Footer = () => {
             <div>
                 <h4>We are always here to help</h4>
                 <p>Committed to your needs, always standing by.</p>
-                
+            </div>
+            <div>
+                <div>
+                    <FaLocationDot/>
+                    <div>
+                        <h5>Location</h5>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     </footer>
